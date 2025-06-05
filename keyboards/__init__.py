c3d4070c9a02c1c271fb478429be63cf5a1696be
VFeduk <my.dream2485@gmail.com>
2025-06-05 18:36:02 +0700

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,1 +1,47 @@
-# Пустой файл для обозначения директории как пакета Python
+# Исправленный keyboards/__init__.py
+# Объединяем все клавиатуры в единой точке импорта
+
+from .main_menu import (
+    get_start_keyboard,
+    get_main_menu_keyboard, 
+    get_city_keyboard,
+    get_gender_keyboard,
+    get_profile_keyboard,
+    get_edit_profile_keyboard,
+    get_payment_methods_keyboard,
+    get_knowledge_keyboard,
+    get_rating_keyboard,
+    get_stars_keyboard
+)
+
+# ИСПРАВЛЕНО: Импорт из правильного файла event_creation.py
+from .event_creation import (
+    get_event_creation_rules_keyboard,
+    get_confirmation_keyboard,
+    get_event_purpose_keyboard,
+    get_event_target_audience_keyboard,
+    get_event_age_keyboard,
+    get_event_registration_keyboard
+)
+
+__all__ = [
+    # Main menu keyboards
+    'get_start_keyboard',
+    'get_main_menu_keyboard',
+    'get_city_keyboard', 
+    'get_gender_keyboard',
+    'get_profile_keyboard',
+    'get_edit_profile_keyboard',
+    'get_payment_methods_keyboard',
+    'get_knowledge_keyboard',
+    'get_rating_keyboard',
+    'get_stars_keyboard',
+    
+    # Event creation keyboards  
+    'get_event_creation_rules_keyboard',
+    'get_confirmation_keyboard',
+    'get_event_purpose_keyboard',
+    'get_event_target_audience_keyboard',
+    'get_event_age_keyboard',
+    'get_event_registration_keyboard'
+]
